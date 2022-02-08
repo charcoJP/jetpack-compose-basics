6c03ac45b1ee86a4ccc7a9ea7f441797fe91676c
Daiki Sumimoto <charco2356@gmail.com>
2022-02-08 16:35:38 +0900

    Add 'MyApp' for Reusing composables

--- app/src/main/java/com/example/compose_basics/MainActivity.kt
+++ app/src/main/java/com/example/compose_basics/MainActivity.kt
@@ -19,12 +19,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
+    }
+}
+
+@Composable
+private fun MyApp() {
+    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+        Greeting("Android")
     }
 }
 
@@ -39,6 +43,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeBasicsTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
